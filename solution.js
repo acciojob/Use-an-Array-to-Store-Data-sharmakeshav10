b43bc154324e221f5cadcea8eb21b36c04e2ac59
Keshav Sharma <46739493+sharmakeshav10@users.noreply.github.com>
2022-07-04 23:11:23 +0530

    yourArray changes

--- solution.js
+++ solution.js
@@ -3,5 +3,6 @@ let yourArray;
 // assign your array the correct value in the next line
 // your code here!
 // code end
+yourArray = ["Chocolate", 50, true, {name: "John", age:20}, "blue"];
 
 module.exports = yourArray
